89fda48c29278feac7f6f0ad9d4ca40d2f389bf5
company-e <company.may.28@gmail.com>
2019-05-06 21:32:42 +0900

    modified vecter.h

---
+++ vector.h
@@ -0,0 +1,6 @@
+#ifndef _INC_GUARD_VECTOR_H_
+#define _INC_GUARD_VECTOR_H_
+
+float compute_inner_product( const float *vector_a, const float *vector_b, int len ); 
+
+#endif /* _INC_GUARD_VECTOR_H */
